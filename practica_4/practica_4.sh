930a8785495d16e8ce3c81313c344b75bc8cd474
patribriyus <735576@unizar.es>
2020-05-03 13:23:16 +0200

    Patri: ficheros actualizados

--- practica_4/practica_4.sh
+++ practica_4/practica_4.sh
@@ -11,21 +11,21 @@ anyadirU() {
 	while read user pass nombre invalid	
 	do
 		# Si alguno de los 3 campos es igual a la cadena vacia se abortará la ejecución
-		(test -z "$user" || test -z "$pass" || test -z "$nombre") && echo "Campo invalido" && exit 1
+		test -z "$user" && test -z "$pass" && test -z "$nombre" && echo "Campo invalido" && exit 1
 		
 		# Si el usuario existe, ni se añadirá ni se cambiará su contraseña
-		getent passwd "$user">/dev/null
+		ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "sudo getent passwd "$user">/dev/null"
 		if [ $? -eq 0 ]; then
 			echo "El usuario $user ya existe"
 		else		
 			# Los usuarios deberán tener un UID mayor o igual que 1815, el directorio home de cada usuario se inicializará con los ficheros de /etc/skel, tendrá como grupo por defecto uno con su mismo nombre
-			useradd -d "/home/$user" -U -K UID_MIN=1815 -c "$nombre" -m -k /etc/skel "$user"
+			ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "useradd -d "/home/$user" -U -K UID_MIN=1815 -c "$nombre" -m -k /etc/skel "$user""
 
 			# Establece su contraseña
-			echo "$user:$pass" | chpasswd
+			ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "echo "$user:$pass" | chpasswd"
 
 			# La caducidad de la nueva contraseña establecida será de 30 días
-			chage -M 30 "$user"
+			cssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "hage -M 30 "$user""
 			echo "$nombre ha sido creado"
 		fi
 
@@ -36,23 +36,23 @@ anyadirU() {
 }
 
 eliminarU() {
-	# Se debe crear el directorio /extra/backup aunque no se borre ningun usuario
-	mkdir -p "/extra/backup"&>/dev/null
+	# Se debe crear el directorio /extra/backupp aunque no se borre ningun usuario
+	ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "mkdir -p "/extra/backup"&>/dev/null"
 
 	for usuario in $(cut -d ',' -f1 "$1")
 	do
 		# comprueba si existe el usuario
-		getent passwd "$usuario">/dev/null
+		ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "getent passwd "$usuario">/dev/null"
 		if [ $? -eq 0 ]; then
 			# guardamos en user_home el directorio
-			user_home=$(getent passwd "$usuario" | cut -d':' -f6)		
+			user_home=$(ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "getent passwd "$usuario"" | cut -d':' -f6)		
 
 			# bloquea la cuenta del usuario para que no pueda acceder
-			usermod -L "$usuario"
+			ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "usermod -L "$usuario""
 	
 			# Antes de borrar un usuario, realizará un backup de su directorio home y se guardara en /extra/backup
 			# En caso de que el backup no pueda ser completado satisfactoriamente, no se realizará el borrado
-			tar -cf "/extra/backup/$usuario.tar" "$user_home" &>/dev/null && userdel -rf "$usuario"&>/dev/null
+			ssh -i ~/.ssh/id_as_ed25519 -n as@"$2" "tar -cf "/extra/backup/$usuario.tar" "$user_home" &>/dev/null && userdel -rf "$usuario"&>/dev/null"
 		fi
 	done<"$1"
 }
@@ -60,14 +60,20 @@ eliminarU() {
 # Comprobación de usuario con privilegios de administración
 test $EUID != 0 && echo "Este script necesita privilegios de administracion" && exit 1
 
-# Cuando el número de argumentos sea distinto de 2 el script termina
-test $# -ne 2 && echo "Numero incorrecto de parametros" && exit 1
+# Cuando el número de argumentos sea distinto de 3 el script termina
+test $# -ne 3 && echo "Numero incorrecto de parametros" && exit 1
 
-# Cualquier otra opción generará el mensaje de error por la salida de error, stderr.
-if [ "$1" = "-a" ]; then
-	anyadirU "$2"
-elif [ "$1" = "-s" ]; then
-	eliminarU "$2"
-else
-	echo "Opcion invalida" 2>&1
-fi
+while read ip
+do
+	# Si el puerto de la dirección ip está cerrado no puede conectarse a una máquina
+	! nc -z $ip 22 2>/dev/null && echo "$ip no es accesible" && exit 1
+
+	# Cualquier otra opción generará el mensaje de error por la salida de error, stderr.
+	if [ "$1" = "-a" ]; then
+		anyadirU "$2"
+	elif [ "$1" = "-s" ]; then
+		eliminarU "$2"
+	else
+		echo "Opcion invalida" 2>&1
+	fi
+done<"$3"
